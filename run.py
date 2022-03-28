c99e32f628c313cb07083bca8969d8e712a1550d
Dante-Cadiz <dantecadiz98@mail.com>
2022-03-28 13:28:47 +0000

    add part of compare input function

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import random
-word_list = ['ace', 'act', 'age', 'ago', 'air', 'ale', 'all', 'and', 'ape', 'app', 'are', 'ark', 'arm', 'ash', 'ask', 'ate', 'bad', 'bag', 'bat', 'bay', 'bed', 'bee', 'bet', 'big', 'bin', 'bit', 'bob', 'bog', 'bop', 'box', 'boy', 'bra', 'bum', 'bun', 'bus', 'but', 'buy', 'bye', 'cab', 'cap', 'can', 'car', 'cat', 'cop', 'cow', 'cry', 'cub', 'cut', 'dab', 'dad', 'dam', 'day', 'den', 'did', 'dip', 'doe', 'dog', 'dot', 'due', 'dug', 'ear', 'eat', 'egg', 'elf', 'elk', 'end', 'eye', 'fan', 'far', 'fat', 'fee', 'few', 'fig', 'fin', 'fit', 'fix', 'fly', 'foe', 'fog', 'for', 'fox', 'fry', 'fun', 'gap', 'gas', 'gay', 'gel', 'get', 'god', 'got', 'had', 'ham', 'has', 'hat', 'hen', 'her', 'hex', 'him', 'hip', 'his', 'hit', 'hoe', 'how', 'ice', 'ill', 'ink', 'jab', 'jam', 'jar', 'jet', 'job', 'jog', 'jug', 'key', 'kit', 'lay', 'leg', 'let', 'lie', 'lip', 'lit', 'lob', 'lot', 'mad', 'man', 'map', 'mat', 'may', 'met', 'mew', 'mix', 'mom', 'mud', 'mug', 'mum', 'nap', 'net', 'net', 'new', 'nod', 'not', 'now', 'nut', 'oaf', 'oar', 'odd', 'oil', 'old', 'one', 'our', 'out', 'owl', 'own', 'pat', 'paw', 'peg', 'pet', 'pin', 'pit', 'pop', 'pot', 'pup', 'put', 'rag', 'ram', 'rap', 'rat', 'row', 'rub', 'rug', 'run', 'sat', 'saw', 'see', 'set', 'sir', 'sit', 'sob', 'sow', 'tan', 'tap', 'tea', 'ten', 'the', 'tip', 'toe', 'top', 'tow', 'tub', 'tug', 'two', 'use', 'van', 'vat', 'vet', 'war', 'was', 'way', 'wet', 'who', 'why', 'wig', 'win', 'won', 'wow', 'yak', 'yes', 'yet', 'you', 'zap', 'zip']
+word_list = ['ace', 'act', 'age', 'ago', 'air', 'ale', 'all', 'amp', 'and', 'ant', 'ape', 'app', 'are', 'ark', 'arm', 'ash', 'ask', 'ate', 'awe', 'axe', 'bad', 'bag', 'bap', 'bat', 'bay', 'bed', 'bee', 'beg', 'bet', 'big', 'bin', 'bit', 'bob', 'bog', 'bop', 'bow', 'box', 'boy', 'bra', 'bud', 'bug', 'bum', 'bun', 'bus', 'but', 'buy', 'bye', 'cab', 'cap', 'can', 'car', 'cat', 'cob', 'cop', 'cow', 'cry', 'cub', 'cut', 'dab', 'dad', 'dam', 'day', 'den', 'did', 'dip', 'doe', 'dog', 'dot', 'dry', 'due', 'dug', 'ear', 'eat', 'egg', 'elf', 'elk', 'end', 'eye', 'fan', 'far', 'fat', 'fee', 'few', 'fig', 'fin', 'fit', 'fix', 'fly', 'foe', 'fog', 'for', 'fox', 'fry', 'fun', 'gap', 'gas', 'gay', 'gel', 'get', 'god', 'got', 'had', 'hag', 'ham', 'has', 'hat', 'hay', 'hen', 'her', 'hex', 'hid', 'him', 'hip', 'his', 'hit', 'hob', 'hoe', 'hop', 'how', 'hub', 'hug', 'hut', 'ice', 'ill', 'ink', 'jab', 'jam', 'jar', 'jaw', 'jet', 'job', 'jog', 'jug', 'jut', 'key', 'kid', 'kit', 'lab', 'lag', 'lap', 'lay', 'leg', 'let', 'lie', 'lip', 'lit', 'lob', 'lot', 'mad', 'man', 'map', 'mat', 'may', 'men', 'met', 'mew', 'mix', 'mob', 'mom', 'mop', 'mud', 'mug', 'mum', 'nag', 'nap', 'net', 'new', 'nod', 'not', 'now', 'nut', 'oaf', 'oar', 'odd', 'oil', 'old', 'one', 'our', 'out', 'owl', 'own', 'pat', 'paw', 'pea', 'peg', 'pet', 'pig', 'pin', 'pit', 'pop', 'pot', 'pup', 'put', 'rag', 'ram', 'rap', 'rat', 'row', 'rub', 'rug', 'run', 'sat', 'saw', 'see', 'set', 'sir', 'sit', 'sob', 'sow', 'tan', 'tap', 'tea', 'ten', 'the', 'tip', 'toe', 'top', 'tow', 'tub', 'tug', 'two', 'use', 'van', 'vat', 'vet', 'war', 'was', 'way', 'wet', 'who', 'why', 'wig', 'win', 'won', 'wow', 'yak', 'yes', 'yet', 'you', 'zap', 'zip']
 
 
 class Game:
@@ -16,7 +16,7 @@ class Game:
         """
         Describes the game
         """
-        return f'Word {self.size} letters long. {self.guesscount} guesses made.'
+        return f'{self.guesscount} guesses made.'
 
 def launch_game():
     """
@@ -24,47 +24,56 @@ def launch_game():
     """
     print('launching game!')
     chosen_word = random.choice(word_list)
-    index = [char for char in chosen_word]
-    new_game = Game(3, index, '', 0)
+    new_game = Game(3, chosen_word, '', 0)
     print(new_game.description())
+    print(chosen_word)
 
 def get_user_input():
+    """
+    Prompts user to submit guess in terminal
+    Triggered on initial game launch and after invalid guesses
+    """
     while True:
         print('Guess a 3 letter word')
-        user_guess = input('Make your guess here: ')
-        user_guess_letters = user_guess.split()
+        guess = input('Make your guess here: ')
         
-        if check_valid_input(user_guess):
+        if check_valid_input(guess):
             print('Guess made')
+
             break
 
-    return user_guess_letters
+    return guess
 
-def check_valid_input(guessed_word):
+def check_valid_input(user_guess):
+    """
+    Checks whether user input is a valid word in the word_list list
+    """
     try:
-        if guessed_word not in word_list:
+        if user_guess not in word_list:
             raise ValueError(
-            f'Your guess {guessed_word} was not in the word list'
+            f'Your guess {user_guess} was not in the word list'
             )
     except ValueError as e:
-       print(f"Invalid data: {e}, please try again.\n")
+       print(f"{e}, please try again.\n")
        return False
 
     return True
 
-def compare_input(word_list, ):
-    # [i for i, j in zip(a, b) if i == j] from stack overflow https://stackoverflow.com/questions/1388818/how-can-i-compare-two-lists-in-python-and-return-matches
-# checks if letter is in correct position
+def compare_input(chosen_word, user_guess):
+    """
+    Compares user input with the random word generated from the list. 
+    Finds position-dependent letter matches and feeds back to user
+    """
+    index = [char for char in chosen_word]
+    guessed_index = user_guess.split()
+    correct_position_guesses = [i for i, j in zip(index, guessed_index) if i == j] 
+    print(f'You guessed the letters {correct_position_guesses} correctly')
 
 
-#def main():
-    #runs the game, generating random word from list as goal word by calling an instance of the class Word
-    #checks that user input is valid - is a word on the word-pool list
-    #indexes across user guess, splitting it into string
-    
+def main():
+    new_game = launch_game()
+    user_guess = get_user_input()
+    result = compare_input()
 
 
-launch_game()
-get_user_input()
-
-
+main()
